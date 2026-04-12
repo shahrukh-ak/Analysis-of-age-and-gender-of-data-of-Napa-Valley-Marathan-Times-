aeee2d1d37234344eaaa7e258bd187d2db6af836
shahrukh-ak <55116845+shahrukh-ak@users.noreply.github.com>
2026-04-12 00:14:07 -0400

    Update code.R

--- code.R
+++ code.R
@@ -1,3 +1,14 @@
+# Analysis-of-age-and-gender-of-data-of-Napa-Valley-Marathan-Times
+# Napa Valley Marathon Times by Age and Gender for 2015 <br>
+## Objectives:
+# - Import the data in R<br>
+# - How many runners are older than 50 years of age?<br>
+# - Display the age distributions of the runner by gender.<br>
+# - Are men older than women?<br>
+# - The average completion time for all runners is 4.361 hours. Test whether the completion time for men is lower than 4.361 hours. <br>
+# - The average age for all runners is 41.33 years. Test whether women are younger than 41.33 years. 
+
+
 q2=read.csv("C:\\Users\\Desktop\\q2.csv")
 head(q2)
   Gender Age  Hours    mph finish_all
